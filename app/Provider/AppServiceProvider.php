56728b2dc32c2695f5e79c193c2249ff1296551a
Maris Mols <maris9229@gmail.com>
2017-08-23 22:53:53 +0300

    Add app providers

---
+++ app/Provider/AppServiceProvider.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace Todo;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class AppServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [];
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}

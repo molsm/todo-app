0cbb116dc2a936800c740a628ae1cf0eb04a4261
Maris Mols <maris9229@gmail.com>
2017-08-23 21:34:41 +0300

    Setup basic app architecture

---
+++ app/Application.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace Todo;
+
+class Application
+{
+    public function start()
+    {
+        var_dump(123);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
-<?php
-phpinfo();
+<?php declare(strict_types = 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var \Todo\Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->start();

44b202ae5da57a882855de38a27f7a7da228e35a
Maris Mols <maris9229@gmail.com>
2017-08-30 20:40:26 +0300

    Add vue js lib

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,11 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var include = require('gulp-include');
 
 gulp.task('sass', function () {
+    console.log(' -- gulp is compiling SASS ');
+
     return gulp.src('./resources/assets/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
@@ -15,13 +18,22 @@ gulp.task('sass', function () {
 });
 
 gulp.task('scripts', function() {
+    console.log(' -- gulp is moving js file');
+
     gulp.src('./resources/assets/js/**/*.js')
         .pipe(concat('app.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./public/js'))
+        .pipe(gulp.dest('./public/js'));
+
+    gulp.src('node_modules/vue/dist/vue.js')
+        .pipe(include())
+        .on('error', console.log)
+        .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('sass:watch', function () {
+
+
+gulp.task('watch', function () {
     gulp.watch('./resources/assets/sass/**/*.scss', ['sass']);
 });
 

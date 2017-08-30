c5c449262589d99328da645f2bcb7d77677dac63
Maris Mols <maris9229@gmail.com>
2017-08-30 20:15:13 +0300

    Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return gulp.src('./resources/assets/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./map'))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./resources/assets/sass/**/*.scss', ['sass']);
+});
 
 gulp.task('default', function() {
-    // place code for your default task here
+    gulp.start('sass');
 });
+
